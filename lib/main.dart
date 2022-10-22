30994d72f88627b0b36c6e6c715824ac0275d8f2
younghanChoi <dnflwlq373@naver.com>
2022-10-22 17:56:04 +0900

    How to use the text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,18 @@ void main() {
   runApp(const MyApp()); // 앱 구동시켜 달라는 의미 // runApp 안에 메인페이지를 입력해줌
 }
 
-// stless 하고 탭 누른 다음 MyApp 입력해주면 메인 페이지 세팅은 끝
-// 코드는 Widget build의 return 뒤에 작성할 것임
+// Flutter에서 앱을 만드는 것은 위젯을 짜깁기 하는 것이라 보면 됨.
+/*
+Flutter에는 위젯이 4개가 있다.
+글자 위젯, 이미지 위젯, 아이콘 위젯, 박스 위젯
+ */
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Text('안녕') // 글자 위젯을 사용하고 싶을때는 Text라는 위젯을 사용
+    );
   }
 }

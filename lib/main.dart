0fc4cc95f7d20d73da2544e842e6d9c8b53bad44
younghanChoi <dnflwlq373@naver.com>
2022-10-23 01:44:21 +0900

    Change to the icon button on the appBar (상단바 아이콘 버튼으로 변경)

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,6 @@ void main() {
   runApp(const MyApp()); // 앱 구동시켜 달라는 의미 // runApp 안에 메인페이지를 입력해줌
 }
 
-// Flutter에서 앱을 만드는 것은 위젯을 짜깁기 하는 것이라 보면 됨.
-/*
- # Flutter에는 위젯이 4개가 있다.
- # 글자 위젯, 이미지 위젯, 아이콘 위젯, 박스 위젯
- # 위젯 디자인을 구글 or 커스텀 → MaterialApp, 아이폰 디자인이면 CupertinoApp 하면 됨
- */
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -19,12 +13,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold( // Scaffold는 앱을 상 중 하 나눠주는 위젯
         appBar: AppBar(
-          leading: Icon(Icons.menu),
-          title: Text('어플리케이션'),
-          actions: [
-            Icon(Icons.search), // 검색 아이콘
-            Icon(Icons.shopping_cart), // 장바구니 아이콘
-            Icon(Icons.notifications) // 알림(종) 아이콘
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: null) , // 좌측에 배치
+          title: TextButton(onPressed: null, child: Text('어플리케이션')), // 텍스트 버튼
+          actions: [ // 우측에 배치
+            IconButton(icon: Icon(Icons.search), onPressed: null), // 검색 아이콘
+            IconButton(icon: Icon(Icons.shopping_cart), onPressed: null), // 장바구니 아이콘
+            IconButton(icon: Icon(Icons.notifications), onPressed: null) // 알림(종) 아이콘
           ],
         ),
         body: Container(
@@ -57,6 +51,5 @@ class MyApp extends StatelessWidget {
         ),
       )
     );
-
   }
 }

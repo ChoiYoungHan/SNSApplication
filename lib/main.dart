bc21bac74b5501e4205ebcee3dfbd7f0e414321c
younghanChoi <dnflwlq373@naver.com>
2022-10-22 18:59:02 +0900

    How to use Scaffold's Row (Scaffold의 가로배치 사용법)

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold( // Scaffold는 앱을 상 중 하 나눠주는 위젯
-        appBar: AppBar(), // 상단
-        body: Container(), // 중앙
-        bottomNavigationBar: BottomAppBar(), // 하단
+        body: Row( // 여러 위젯을 가로로 배치하는법
+          children: [
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star),
+          ],
+        ),
       ),
     );
   }

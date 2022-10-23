67cac3ad85cf731a2610ab863047dd43375b53b1
younghanChoi <dnflwlq373@naver.com>
2022-10-23 22:28:25 +0900

    2022 10 23 마무리

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return new Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text('친구', style: TextStyle(fontWeight: FontWeight.w300, color: Color(
@@ -35,45 +35,42 @@ class HomeScreen extends StatelessWidget{
         ],
       ),
       body: Container(
-        height: 150, width: double.infinity, // 세로 150, 가로 최대
-        padding: EdgeInsets.all(20), // 여백 20
-        child: Row( // 가로 배치
+        height: 100, // 세로 150
+        child: Row(
           children: [
-            Flexible(child: Image.asset('assets/Camera.jpg', width: 150), flex: 3), // 사진불러오는 것 비율은 3
+            Flexible(child: ClipRRect(
+              borderRadius: BorderRadius.circular(35),
+              child: Image.asset('assets/Camera.jpg', width: 100, height: 100)), flex: 2
+            ),
             Flexible(child: Container(
-              width: double.infinity, // 가로길이를 최대로
-              padding: EdgeInsets.all(10), // 여백 10
+              width: double.infinity,
+              padding: EdgeInsets.all(10),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽정렬
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Flexible(child: Text('카메라 팝니다'), flex: 1),
-                  Flexible(child: Text('금호동 3가'), flex: 1),
-                  Flexible(child: Text('7000원'), flex: 1),
-                  Flexible(child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Icon(Icons.favorite),
-                      Text('4')
-                    ],
-                  ), flex: 1)
+                  Flexible(child: Text('이름'), flex: 1),
+                  Flexible(child: Text('상태메시지'), flex: 1)
                 ],
               ),
-            ), flex: 7)
+            ), flex: 8
+            )
           ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Row( // 가로로 정렬
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 간격을 두고 정렬
-          children: [
-            IconButton(onPressed: null, icon: Icon(Icons.person_outline)), // 친구목록 아이콘
-            IconButton(onPressed: null, icon: Icon(Icons.chat_bubble_outline)), // 채팅목록 아이콘
-            IconButton(onPressed: null, icon: Icon(Icons.list_alt)), // 게시글목록 아이콘
-            IconButton(onPressed: null, icon: Icon(Icons.segment)) // 전체목록 아이콘
-          ],
+        child: Container( // 가로로 정렬
+          height: 60,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 간격을 두고 정렬
+            children: [
+              IconButton(onPressed: null, icon: Icon(Icons.person_outline)), // 친구목록 아이콘
+              IconButton(onPressed: null, icon: Icon(Icons.chat_bubble_outline)), // 채팅목록 아이콘
+              IconButton(onPressed: null, icon: Icon(Icons.list_alt)), // 게시글목록 아이콘
+              IconButton(onPressed: null, icon: Icon(Icons.segment)) // 전체목록 아이콘
+            ],
+          ),
         ),
       ),
     );
   }
 }
-

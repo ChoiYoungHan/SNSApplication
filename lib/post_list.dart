f1812f940b33ed320e24ecad92a99fbaa071f7a9
younghanChoi <dnflwlq373@naver.com>
2022-11-03 16:09:35 +0900

    Switch to postList index format (postList index 형식으로 전환)

--- lib/post_list.dart
+++ lib/post_list.dart
@@ -5,152 +5,175 @@ import 'package:flutter/services.dart';
 
 import 'my_List.dart';
 
-class post_List extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text('게시글', style: TextStyle(fontWeight: FontWeight.w300, color: Color(
-            0xff797979))), // 글자의 두께를 줄임
-        actions: [ // 우측에 배치
-          IconButton(icon: Icon(Icons.search), onPressed: null), // 검색 아이콘
-          IconButton(icon: Icon(Icons.add_circle_outline_outlined), onPressed: null), // 게시글 추가 아이콘
-        ],
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(10),
-        scrollDirection: Axis.vertical,
-        children: [
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10),
-          postlist_screen(),Container(height: 10)
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 60,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 간격을 두고 정렬
-            children: [
-              IconButton(onPressed: (){
-                Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation1, animation2) => MyApp(), // 즉시 이동을 하고 싶을때는 PageRouteBuilder 사용
-                    transitionDuration: Duration.zero, // 속도 0
-                    reverseTransitionDuration: Duration.zero // 속도 0
-                )); // 친구 목록으로 이동
-              }, icon: Icon(Icons.person_outline)), // 친구목록 아이콘
-              IconButton(onPressed: () {
-                Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation1, animation2) => chat_List(), // 즉시 이동을 하고 싶을때는 PageRouteBuilder 사용
-                    transitionDuration: Duration.zero, // 속도 0
-                    reverseTransitionDuration: Duration.zero // 속도 0
-                )); // 채팅 목록으로 이동
-              }, icon: Icon(Icons.chat_bubble_outline)), // 채팅목록 아이콘
-              IconButton(onPressed: null, icon: Icon(Icons.list_alt, color: Colors.blue)), // 게시글목록 아이콘
-              IconButton(onPressed: (){
-                Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation1, animation2) => my_List(), // 즉시 이동을 하고 싶을때는 PageRouteBuilder 사용
-                    transitionDuration: Duration.zero, // 속도 0
-                    reverseTransitionDuration: Duration.zero // 속도 0
-                )); // 전체 목록으로 이동
-              }, icon: Icon(Icons.segment)) // 전체목록 아이콘
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class  postlist_screen extends StatelessWidget {
-  const postlist_screen({Key? key}) : super(key: key);
+class post_List extends StatelessWidget {
+  const post_List({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container( // 박스
-      width: double.infinity, //
-      height: 400, // 높이 400
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 1.5,
-          color: Color(0xffC6C8C6)
-        ),
-      ),
-      child: Column( // 세로로 정렬
-        children: [ // Container 3개 줄 것임
-          Expanded(child: Container(
-            width: double.infinity, height: 40,
-            child: Row( // 가로로 정렬 Container 2개 줄 것임
-              children: [
-                Expanded(child: Container(
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(45),
-                      child: Image.asset('assets/sky.jpg', width: 100, height: 100, fit: BoxFit.cover),
-                    ),
-                  )
-                ), flex: 2),
-                Expanded(child: Container(
-                  child: Column( // Container 2개가 들어갈 것임
-                    children: [
-                      Expanded(child: Container(
-                        child: Row( // Text와 Icon 2개가 들어갈 것임
-                          children: [
-                            Expanded(child: Container(
-                              child: Text('최영한', style: TextStyle(fontSize: 16)),
-                            ), flex: 2),
-                            Expanded(child: Container(
-                              alignment: Alignment.centerRight,
-                              child: Row( // 가로로 Icon 2개 넣어줄 것임
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  IconButton(onPressed: null, icon: Icon(Icons.more_horiz, color: Color(0xffC6C8C6))),
-                                  IconButton(onPressed: null, icon: Icon(Icons.close, color: Color(0xffC6C8C6)))
-                                ],
-                              ),
-                            ), flex: 8)
-                          ],
-                        ),
-                      ), flex: 1),
-                      Expanded(child: Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text('3시간 전', style: TextStyle(fontSize: 13)),
-                      ), flex: 1)
-                    ],
-                  ),
-                ), flex: 8)
-              ],
-            ),
-            decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(
-                width: 1.5,
-                color: Color(0xffC6C8C6)
-              ))
-            ),
-          ), flex: 1),
-          Expanded(child: Container(
-            width: double.infinity, height: 80,
-            padding: EdgeInsets.all(3),
-            child: Text('텍스트가 출력될 영역'),
-          ), flex: 2),
-          Expanded(child: Container(
-            width: double.infinity, height: 280,
-            padding: EdgeInsets.all(1.5),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(13),
-              child: Image.asset('assets/sky.jpg', width: 100, height: 100, fit: BoxFit.cover), // 사진으로 화면을 꽉 채움
-            )
-          ), flex: 7)
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListViewPage()
     );
   }
 }
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+
+  var friendImage = ['assets/Ahnhyunsoo.png', 'assets/Choihojin.png', 'assets/Kimwon.png'];
+  var friendName = ['Ahnhyunsoo', 'Choihojin', 'Kimwon'];
+  var postTime = ['1분전', '1시간 전', '1일 전'];
+  var postContents = ['밤하늘 보러가실 분~', '안녕하세요.', '깃허브 봐바'];
+  var postImage = ['assets/sky.jpg', 'assets/Camera.jpg', 'assets/Github_Image.jpg'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('게시글', style: TextStyle(fontWeight: FontWeight.w300, color: Color(0xff797979))),
+        actions: [
+          IconButton(onPressed: null, icon: Icon(Icons.search)),
+          IconButton(onPressed: null, icon: Icon(Icons.add_circle_outline_outlined))
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: postContents.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onLongPress: (){},
+            child: Container(
+              width: double.infinity, height: 450,
+              padding: EdgeInsets.all(5),
+              child: Card(
+               child: Column(
+                 mainAxisSize: MainAxisSize.max,
+                 children: [
+                   Expanded(child: Container(
+                     width: double.infinity, height: double.infinity,
+                       decoration: BoxDecoration(
+                           border: Border(bottom: BorderSide(
+                               width: 1.5,
+                               color: Color(0xffC6C8C6)
+                           ))
+                       ),
+                     child: Row(
+                       mainAxisSize: MainAxisSize.max,
+                       children: [
+                         Expanded(child: Container(
+                           width: double.infinity, height: double.infinity,
+                           child: Padding(
+                             padding: EdgeInsets.all(3),
+                             child: ClipRRect(
+                               borderRadius: BorderRadius.circular(35),
+                               child: Image.asset(friendImage[index], width: 100, height: 100, fit: BoxFit.cover)
+                             )
+                           )
+                         ), flex: 2),
+                         Expanded(child: Container(
+                           width: double.infinity, height: double.infinity,
+                           child: Column(
+                             mainAxisSize: MainAxisSize.max,
+                             children: [
+                               Expanded(child: Container(
+                                 width: double.infinity, height: double.infinity,
+                                 child: Row(
+                                   mainAxisSize: MainAxisSize.max,
+                                   children: [
+                                     Expanded(child: Container(
+                                       width: double.infinity, height: double.infinity,
+                                       padding: EdgeInsets.fromLTRB(3, 6, 0, 0),
+                                       child: Text(friendName[index]),
+                                     ), flex: 3),
+                                     Expanded(child: Container(
+                                       width: double.infinity, height: double.infinity,
+                                       padding: EdgeInsets.fromLTRB(0, 6, 3, 0),
+                                       child: Row(
+                                         mainAxisSize: MainAxisSize.max,
+                                         mainAxisAlignment: MainAxisAlignment.end,
+                                         children: [
+                                           Icon(Icons.more_horiz, color: Color(0xffC6C8C6)),
+                                           Padding(
+                                             padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                                             child: Icon(Icons.close, color: Color(0xffC6C8C6))
+                                           )
+                                         ],
+                                       )
+                                     ), flex: 8)
+                                   ],
+                                 )
+                               ), flex: 1),
+                               Expanded(child: Container(
+                                 width: double.infinity, height: double.infinity,
+                                 padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
+                                 child: Text(postTime[index], style: TextStyle(color: Color(0xffC6C8C6)))
+                               ), flex: 1)
+                             ]
+                           )
+                         ), flex: 11)
+                       ]
+                     )
+                   ), flex: 1),
+                   Expanded(child: Container(
+                     width: double.infinity, height: double.infinity,
+                     padding: EdgeInsets.all(10),
+                     child: Text(postContents[index], maxLines: 3),
+                   ), flex: 2),
+                   Expanded(child: Container(
+                     width: double.infinity, height: double.infinity,
+                     child: Image.asset(postImage[index], width: 100, height: 100, fit: BoxFit.cover),
+                   ), flex: 5)
+                 ],
+               )
+              )
+            )
+          );
+        }
+      ),
+      bottomNavigationBar: BottomAppBar( // 하단바
+        child: Container(
+          height: 60,
+          child: Row( // 가로로 정렬
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 간격을 두고 정렬
+            children: [
+              IconButton(
+                  onPressed: (){
+                    Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation1, animation2) => MyApp(), // 즉시 이동을 하고 싶을때는 PageRouteBuilder 사용
+                        transitionDuration: Duration.zero, // 속도 0
+                        reverseTransitionDuration: Duration.zero // 속도 0
+                    )); // 친구 목록으로 이동
+                  },
+                  icon: Icon(Icons.person_outline)), // 친구목록 아이콘버튼
+              IconButton(
+                  onPressed: (){
+                    Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation1, animation2) => chat_List(), // 즉시 이동을 하고 싶을때는 PageRouteBuilder 사용
+                        transitionDuration: Duration.zero, // 속도 0
+                        reverseTransitionDuration: Duration.zero // 속도 0
+                    )); // 채팅 목록으로 이동
+                  },
+                  icon: Icon(Icons.chat_bubble_outline)), // 채팅목록 아이콘버튼
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.list_alt, color: Colors.blue)), // 게시글목록 아이콘버튼
+              IconButton(
+                  onPressed: (){
+                    Navigator.push(context, PageRouteBuilder(pageBuilder: (context, animation1, animation2) => my_List(), // 즉시 이동을 하고 싶을때는 PageRouteBuilder 사용
+                        transitionDuration: Duration.zero, // 속도 0
+                        reverseTransitionDuration: Duration.zero // 속도 0
+                    )); // 전체 목록으로 이동
+                  },
+                  icon: Icon(Icons.segment)), // 전체목록 아이콘버튼
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+

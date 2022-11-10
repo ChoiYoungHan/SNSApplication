762c5f638c86008b2f87d76e7bbf201911f68b29
younghanChoi <dnflwlq373@naver.com>
2022-11-11 01:30:00 +0900

    Fixed an error that occurred when the chat bar was uploaded (채팅바가 올라올 때 오류 발생하던 것 해결)

--- lib/post_main.dart
+++ lib/post_main.dart
@@ -16,6 +16,7 @@ class post_Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // 채팅바 올라올 때 화면 밀림 방지
       appBar: AppBar(
         backgroundColor: Colors.white,
         leading: IconButton(
@@ -24,109 +25,112 @@ class post_Main extends StatelessWidget {
         ),
         title: Text(post_Data.friendName, style: TextStyle(color: Colors.grey, fontSize: 20, fontWeight: FontWeight.bold)),
       ),
-      body: GestureDetector(
-        child: Container(
-          width: double.infinity, height: double.infinity,
+      body: SafeArea(
+        child: GestureDetector(
+          child: Container(
+            width: double.infinity, height: double.infinity,
             child: Column(
               mainAxisSize: MainAxisSize.max,
-                children: [
-                  Container(
-                    width: double.infinity, height: 450,
-                    child: Card(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Expanded(child: Container(
-                            width: double.infinity, height: double.infinity,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  Expanded(child: Container(
-                                    child: Padding(
-                                      padding: EdgeInsets.all(10),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(35),
-                                        child: Image.asset(post_Data.friendImage)
+              children: [
+                Expanded(child: Container(
+                  width: double.infinity, height: double.infinity,
+                  child: Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(child: Container(
+                          width: double.infinity, height: double.infinity,
+                          child: Row(
+                            children: [
+                              Expanded(child: Container(
+                                width: double.infinity, height: double.infinity,
+                                child: Padding(
+                                  padding: EdgeInsets.all(5),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(35),
+                                    child: Image.asset(post_Data.friendImage)
+                                  )
+                                )
+                              ), flex: 2),
+                              Expanded(child: Container(
+                                width: double.infinity, height: double.infinity,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(child: Container(
+                                      width: double.infinity, height: double.infinity,
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.max,
+                                        children: [
+                                          Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                          child: Text(post_Data.friendName,
+                                            style: TextStyle(fontWeight: FontWeight.bold))),
+                                          Expanded(child: Padding(
+                                            padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                            child: Align(
+                                              alignment: Alignment.centerRight,
+                                              child: IconButton(
+                                                onPressed: null,
+                                                icon: Icon(Icons.more_horiz)
+                                              )
+                                            )
+                                          ))
+                                        ]
                                       )
-                                    )
-                                  ), flex: 2),
-                                  Expanded(child: Container(
-                                    width: double.infinity, height: double.infinity,
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.max,
-                                      children: [
-                                        Expanded(child: Container(
-                                          width: double.infinity, height: double.infinity,
-                                          child: Row(
-                                            mainAxisSize: MainAxisSize.max,
-                                            children: [
-                                              Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                                                child: Text(post_Data.friendName,
-                                                style: TextStyle(fontWeight: FontWeight.bold))),
-                                              Expanded(child: Padding(
-                                                padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                                                child: Align(
-                                                  alignment: Alignment.centerRight,
-                                                  child: IconButton(
-                                                    onPressed: null,
-                                                    icon: Icon(Icons.more_horiz)
-                                                  ),
-                                                )
-                                              ))
-                                            ]
-                                          )
-                                        )
-                                      ),
+                                    ), flex: 1),
                                     Expanded(child: Container(
                                       width: double.infinity, height: double.infinity,
                                       padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                                      child: Text(post_Data.postTime, style: TextStyle(color: Colors.grey, fontSize: 12))
-                                    ))
-                                    ]
-                                    )
-                                  ), flex: 11)
-                                ]
-                            )
-                          ), flex: 1),
-                          Expanded(child: Container(
-                            width: double.infinity, height: double.infinity,
-                            child: Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Text(post_Data.postContents)
-                            )
-                          ), flex: 2),
-                          Expanded(child: Container(
-                            width: double.infinity, height: double.infinity,
-                            child: Padding(
-                              padding: EdgeInsets.all(3),
-                              child: Image.asset(post_Data.postImage)
-                              )
-                          ), flex: 5)
-                        ]
-                      )
+                                      child: Text(post_Data.postTime, style: TextStyle(
+                                       color: Colors.grey, fontSize: 12
+                                      ))
+                                    ), flex: 1)
+                                  ]
+                                )
+                              ), flex: 11)
+                            ]
+                          )
+                        ), flex: 1),
+                        Expanded(child: Container(
+                          width: double.infinity, height: double.infinity,
+                          child: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Text(post_Data.postContents)
+                          )
+                        ), flex: 2),
+                        Expanded(child: Container(
+                          width: double.infinity, height: double.infinity,
+                          child: Padding(
+                            padding: EdgeInsets.all(3),
+                            child: Image.asset(post_Data.postImage, fit: BoxFit.cover)
+                          )
+                        ), flex: 5)
+                      ]
                     )
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: comment_writeName.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          child: Container(
+                  )
+                ), flex: 7),
+                Expanded(child: Container(
+                  width: double.infinity, height: double.infinity,
+                  child: ListView.builder(
+                    itemCount: comment_writeName.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        child: Container(
                           width: double.infinity, height: 70,
                           child: Card(
                             child: Row(
                               mainAxisSize: MainAxisSize.max,
                               children: [
-                              Expanded(child: Container(
-                                width: double.infinity, height: double.infinity,
-                                child: Padding(
-                                  padding: EdgeInsets.all(10),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(35),
-                                    child: Image.asset(comment_writeImage[index], width: 100, height: 100, fit: BoxFit.cover),
+                                Expanded(child: Container(
+                                  width: double.infinity, height: double.infinity,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(35),
+                                      child: Image.asset(comment_writeImage[index], fit: BoxFit.cover)
+                                    )
                                   )
-                                )
-                              ), flex: 2),
+                                ), flex: 2),
                                 Expanded(child: Container(
                                   width: double.infinity, height: double.infinity,
                                   child: Column(
@@ -139,8 +143,7 @@ class post_Main extends StatelessWidget {
                                           child: Row(
                                             mainAxisSize: MainAxisSize.max,
                                             children: [
-                                            Text(comment_writeName[index],
-                                              style: TextStyle(fontWeight: FontWeight.bold)),
+                                              Text(comment_writeName[index], style: TextStyle(fontWeight: FontWeight.bold)),
                                               Padding(padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
                                                 child: Text(comment_writeTime[index],
                                                 style: TextStyle(fontSize: 11, color: Colors.grey)))
@@ -158,40 +161,37 @@ class post_Main extends StatelessWidget {
                               ]
                             )
                           )
-                          )
-                        );
-                      })
+                        )
+                      );
+                    })
+                ), flex: 3),
+                Expanded(child: Container(
+                  width: double.infinity, height: double.infinity,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(child: Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        width: double.infinity, height: double.infinity,
+                        child: TextField(
+                          decoration: InputDecoration(hintText: '댓글을 입력해주세요.')
+                        )
+                      ), flex: 8),
+                      Expanded(child: Container(
+                        width: double.infinity, height: double.infinity,
+                        child: IconButton(
+                          onPressed: null,
+                          icon: Icon(Icons.send, color: Colors.blue)
+                        )
+                      ), flex: 2)
+                    ]
                   )
+                ), flex: 1)
               ]
             )
           )
+        )
       )
-      ,bottomNavigationBar: BottomAppBar(
-        child: Container(
-        height: 50,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(child: Container(
-              child: Form(
-              key: this.formKey,
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  decoration: InputDecoration(hintText: '댓글을 입력해주세요.'),
-                )
-              )
-            )
-          ), flex: 8),
-          Expanded(child: Container(
-            padding: EdgeInsets.all(5),
-            child: IconButton(
-              onPressed: null,
-              icon: Icon(Icons.send, color: Colors.blue)
-          )), flex: 2)
-        ]
-      ))
-    )
     );
   }
 }
